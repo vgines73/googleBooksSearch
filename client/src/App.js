9550f775baaa1519ed91b9db9881bff53bbbd95c
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2021-03-06 20:11:51 -0800

    able to get data, but not sure if this is the right one.

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,35 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import axios from "axios";
-import Saved from './Pages/Saved'
-import Search from "./Pages/Search"
-import Books from "./Pages/Books"
+import Saved from "./Pages/Saved";
+import Search from "./Pages/Search";
+import Books from "./Pages/Books";
 import Nav from "./Components/Nav/Nav";
-const API_KEY = require("dotenv")
+const API_KEY = require("dotenv");
 function App() {
   // const [books, setBooks] = useState([])
   const getAll = async () => {
-    const { data } = await axios.get("https://www.googleapis.com/books" + API_KEY) 
+    const { data } = await axios.get(
+      "https://www.googleapis.com/books/v1/volumes?q=search+terms" + API_KEY
+    );
     console.log(data);
-  }
+  };
   useEffect(() => {
     // console.log("effect here");
-    getAll()
-  }, [])
+    getAll();
+  }, []);
   return (
     <div className="App">
       <Nav />
-
       <Router>
         <Switch>
           <Route path="/saved" component={Saved} />
-          <Route path="/search" component={Search}/>
+          <Route path="/search" component={Search} />
           <Route path="/books">
-            <Books/>
+            <Books />
           </Route>
-          <Route path="/" component={Books}/>
+          <Route path="/" component={Books} />
         </Switch>
       </Router>
     </div>
